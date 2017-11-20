bef645021388f7a22ef08740b9fba2109fbae571
Just_pan <317604832@qq.com>
2017-11-20 20:27:05 +0800

    生产环境构建

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import _ from 'lodash'
 import { cube } from './math.js';
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
 function component() {
   // var element = document.createElement('div');
   // var btn = document.createElement('button');
